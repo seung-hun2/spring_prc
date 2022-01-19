c288b181afa7eeb7d9edfc78b30bd0977d8cd712
DESKTOP-9KPQD1P\정승훈 <jungsh2328@naver.com>
2022-01-20 02:12:32 +0900

    1st update join

---
+++ src/main/java/example/demo/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package example.demo.repository;
+
+
+import example.demo.domain.Member;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ src/main/java/example/demo/service/SpringConfig.java
@@ -0,0 +1,34 @@
+package example.demo.service;
+
+import example.demo.repository.JdbcMemberRepository;
+import example.demo.repository.MemberRepository;
+import example.demo.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
+}
